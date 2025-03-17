4d6487010f81e7ecde322962b26fecea89b9454e
amartinezs2021 <a.martinezs.2021@alumnos.urjc.es>
2025-03-17 11:42:19 +0100

    nuevo crono

--- P2/cronoBueno.js
+++ P2/cronoBueno.js
@@ -1,55 +1,64 @@
-//-- Elementos de la gui
-const gui = {
-    display : document.getElementById("display"),
-    start : document.getElementById("start"),
-    stop : document.getElementById("stop"),
-    reset : document.getElementById("reset")
-}
+//-- Clase cronómetro
+class Crono {
 
-console.log("Ejecutando JS...");
+    //-- Constructor. Hay que indicar el 
+    //-- display donde mostrar el cronómetro
+    constructor(display) {
+        this.display = display;
 
-//-- Definir un objeto cronómetro
-const crono = new Crono(gui.display);
-
-//---- Configurar las funciones de retrollamada
-
-//-- Arranque del cronometro
-gui.start.onclick = () => {
-    console.log("Start!!");
-    crono.start();
-}
-  
-//-- Detener el cronómetro
-gui.stop.onclick = () => {
-    console.log("Stop!");
-    crono.stop();
-}
-
-//-- Reset del cronómetro
-gui.reset.onclick = () => {
-    console.log("Reset!");
-    // Generar una nueva clave
-    console.log("Nueva clave")
-    secretkey.length = 0; // Vaciar el array de la clave actual
-    for (let i = 0; i < 4; i++) {
-        let rnum = getRandomInt(10);
-        secretkey.push(rnum.toString());
-        console.log(i + ' Secret Key '  + secretkey[i]);
+        //-- Tiempo
+        this.cent = 0, //-- Centésimas
+        this.seg = 0,  //-- Segundos
+        this.min = 0,  //-- Minutos
+        this.timer = 0;  //-- Temporizador asociado
     }
 
-    crono.stop();
-    crono.reset();
+    //-- Método que se ejecuta cada centésima
+    tic() {
+        //-- Incrementar en una centesima
+        this.cent += 1;
 
-    // Reiniciar la interfaz
-    elemento.clave1.innerHTML = "*";
-    elemento.clave2.innerHTML = "*";
-    elemento.clave3.innerHTML = "*";
-    elemento.clave4.innerHTML = "*";
-    elemento.clave1.style.color = "red";
-    elemento.clave2.style.color = "red";
-    elemento.clave3.style.color = "red";
-    elemento.clave4.style.color = "red";
-  
-    // Volver al estado inicial
-    estado = ESTADO.INIT;
-};
+        //-- 100 centésimas hacen 1 segundo
+        if (this.cent == 100) {
+        this.seg += 1;
+        this.cent = 0;
+        }
+
+        //-- 60 segundos hacen un minuto
+        if (this.seg == 60) {
+        this.min = 1;
+        this.seg = 0;
+        }
+
+        //-- Mostrar el valor actual
+        this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
+    }
+
+    //-- Arrancar el cronómetro
+    start() {
+       if (!this.timer) {
+          //-- Lanzar el temporizador para que llame 
+          //-- al método tic cada 10ms (una centésima)
+          this.timer = setInterval( () => {
+              this.tic();
+          }, 10);
+        }
+    }
+
+    //-- Parar el cronómetro
+    stop() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
+
+    //-- Reset del cronómetro
+    reset() {
+        this.cent = 0;
+        this.seg = 0;
+        this.min = 0;
+
+        this.display.innerHTML = "0:0:0";
+    }
+}

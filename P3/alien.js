9431cff7e0b751469aa7ae31813ea62c120cc2c5
amartinezs2021 <a.martinezs.2021@alumnos.urjc.es>
2025-04-03 19:12:43 +0200

    en proceso

--- P3/alien.js
+++ P3/alien.js
@@ -11,3 +11,34 @@ const victorySound = document.getElementById('victorySound');
 // Tamaño del canvas
 canvas.width = 800;
 canvas.height = 600;
+
+// Variables del juego
+let gameRunning = false;
+let score = 0;
+let lives = 3;
+let lastTime = 0;
+let enemySpawnTimer = 0;
+let enemyShootTimer = 0;
+
+// Imágenes
+const playerImg = new Image();
+playerImg.src = 'nave.png';
+
+const enemyImg = new Image();
+enemyImg.src = 'imagen.png';
+
+// Objetos del juego
+const player = {
+    x: canvas.width / 2,
+    y: canvas.height - 60,
+    width: 50,
+    height: 50,
+    speed: 7,
+    isMovingLeft: false,
+    isMovingRight: false
+};
+
+let bullets = [];
+let enemies = [];
+let enemyBullets = [];
+

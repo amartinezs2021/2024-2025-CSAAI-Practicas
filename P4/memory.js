458292517b644294d3383b9975123db8ac4210a4
amartinezs2021 <a.martinezs.2021@alumnos.urjc.es>
2025-04-21 12:28:50 +0200

    otra funcion

--- P4/memory.js
+++ P4/memory.js
@@ -97,3 +97,15 @@ function iniciarJuego() {
         tablero.appendChild(card);
     });
 }
+
+function mezclarArray(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+

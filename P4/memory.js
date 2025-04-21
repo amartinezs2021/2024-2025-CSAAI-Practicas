954969c8679d7b95b4f924212fb63e74343a2bc3
amartinezs2021 <a.martinezs.2021@alumnos.urjc.es>
2025-04-21 11:35:03 +0200

    comienzo del memory

---
+++ P4/memory.js
@@ -0,0 +1,46 @@
+let modoSeleccionado = 'andrea';
+let dificultadSeleccionada = 2;
+
+const imagenes = {
+    andrea: [
+        'aiko1.jpg',
+        'aiko2.jpg',
+        'aiko3.jpg',
+        'aiko4.jpg',
+        'aiko5.jpg',
+        'aiko6.jpg',
+        'aiko7.jpg',
+        'aiko8.jpg',
+        'aiko9.jpg',
+        'aiko10.jpg',
+        'aiko11.jpg',
+        'aiko12.jpg',
+        'aiko13.jpg',
+        'aiko14.jpg',
+        'aiko15.jpg',
+        'aiko16.jpg',
+        'aiko17.jpg',
+        'aiko18.jpg',
+    ],
+    lola: [
+        
+        'lola1.jpg',
+        'lola2.jpg',
+        'lola3.jpg',
+        'lola4.jpg',
+        'lola5.jpg',
+        'lola6.jpg',
+        'lola7.jpg',
+        'lola8.jpg',
+        'lola9.jpg',
+        'lola10.jpg',
+        'lola11.jpg',
+        'lola12.jpg',
+        'lola13.jpg',
+        'lola14.jpg',
+        'lola15.jpg',
+        'lola16.jpg',
+        'lola17.jpg',
+        'lola18.jpg',
+    ]
+};

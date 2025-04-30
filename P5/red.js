83dafe64d09649760df07365f8cff2d8ac25e1cb
amartinezs2021 <a.martinezs.2021@alumnos.urjc.es>
2025-04-30 12:33:26 +0200

    constantes definidas

--- P5/red.js
+++ P5/red.js
@@ -1,3 +1,16 @@
 const canvas = document.getElementById('networtCanvas');
 const ctx = canvas.getContent('2d');
 
+let redAleatoria;
+let nodoOrigen = 0, nodoDestino = 0;
+let rutaMinimaConRetardos;
+
+const nodeRadius = 40;
+const numNodos = 5;
+const nodeConnect = 2;
+const nodeRandomDelay = 1000;
+const pipeRandomWeight = 100;
+
+const btnCNet = document.getElementById("btnCNet");
+const btnMinPath = document.getElementById("btnMinPath");
+
